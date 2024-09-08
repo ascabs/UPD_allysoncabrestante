6429f3f9de69bc0e740c449e4ea6a23828f7f229
ascabs <ascabrestante@up.edu.ph>
2024-09-08 13:08:58 +0800

    Update

--- create_account.ts
+++ create_account.ts
@@ -1,27 +1,59 @@
-import bs58 from "bs58";
-import {Keypair, PublicKey, SystemProgram, Transaction,Connection, clusterApiUrl} from "@solana/web3.js"
+// import bs58 from "bs58";
+// import {Keypair, PublicKey, SystemProgram, Transaction,Connection, clusterApiUrl} from "@solana/web3.js"
+// (async() => {
+//     const CONNECTION = new Connection(clusterApiUrl("devnet"));
+
+//     const privKey = "Qs36P6uJr2dcTvHKp6aQeKouRRVhMPAspzTJEYmUx9rwjVdNdTa9eEabvsyL1Uq13Wz7WjmbbHa9PBHkHGTNkR2";
+//     const decoded = bs58.decode(privKey);
+
+//     const myKeypair = Keypair.fromSecretKey(decoded);
+//     console.log(myKeypair.publicKey);
+//     console.log(myKeypair.secretKey);
+
+//     const tx = new Transaction();
+//     const newAccKeypair = Keypair.generate();
+//     const rent = await CONNECTION.getMinimumBalanceForRentExemption(0);
+//     tx.instructions = [
+//         SystemProgram.createAccount({
+//             fromPubkey: myKeypair.publicKey,
+//             newAccountPubkey: newAccKeypair.publicKey,
+//             lamports: rent,
+//             space: 0,
+//             programId: SystemProgram.programId
+//         })];
+    
+//     const txHash = await CONNECTION.sendTransaction(tx, [myKeypair,newAccKeypair]);
+//     console.log(txHash);
+// })();
+
+import bs58 from "bs58"
+import {clusterApiUrl, Connection, Keypair, SystemProgram, Transaction} from "@solana/web3.js"
+
 (async() => {
     const CONNECTION = new Connection(clusterApiUrl("devnet"));
+    const { lastValidBlockHeight, blockhash } = await CONNECTION.getLatestBlockhash();
 
-    const privKey = "Qs36P6uJr2dcTvHKp6aQeKouRRVhMPAspzTJEYmUx9rwjVdNdTa9eEabvsyL1Uq13Wz7WjmbbHa9PBHkHGTNkR2";
-    const decoded = bs58.decode(privKey);
+    const myPrivkey = "Qs36P6uJr2dcTvHKp6aQeKouRRVhMPAspzTJEYmUx9rwjVdNdTa9eEabvsyL1Uq13Wz7WjmbbHa9PBHkHGTNkR2";
+    const myKeypair = Keypair.fromSecretKey(bs58.decode(myPrivkey));
 
-    const myKeypair = Keypair.fromSecretKey(decoded);
-    console.log(myKeypair.publicKey);
-    console.log(myKeypair.secretKey);
-
-    const tx = new Transaction();
     const newAccKeypair = Keypair.generate();
-    const rent = await CONNECTION.getMinimumBalanceForRentExemption(0);
+    const tx = new Transaction();
     tx.instructions = [
         SystemProgram.createAccount({
             fromPubkey: myKeypair.publicKey,
             newAccountPubkey: newAccKeypair.publicKey,
-            lamports: rent,
+            lamports: await CONNECTION.getMinimumBalanceForRentExemption(0) ,
             space: 0,
-            programId: SystemProgram.programId
-        })];
-    
-    const txHash = await CONNECTION.sendTransaction(tx, [myKeypair,newAccKeypair]);
+            programId: SystemProgram.programId,
+        })
+    ]
+
+    tx.recentBlockhash = blockhash;
+    tx.lastValidBlockHeight = lastValidBlockHeight
+    tx.sign(myKeypair,newAccKeypair);
+    tx.feePayer = myKeypair.publicKey;
+
+    const txSerialized = tx.serialize();
+    const txHash = await CONNECTION.sendRawTransaction(txSerialized);
     console.log(txHash);
 })();
--- transfer_sol.ts
+++ transfer_sol.ts
@@ -1,24 +1,26 @@
-import bs58 from "bs58";
-import {Keypair, PublicKey, SystemProgram, Transaction,Connection, clusterApiUrl, LAMPORTS_PER_SOL} from "@solana/web3.js"
-(async() => {
+import bs58 from "bs58"
+import {Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, clusterApiUrl} from "@solana/web3.js"
+
+(async()=>{
     const CONNECTION = new Connection(clusterApiUrl("devnet"));
-
-    const privKey = "Qs36P6uJr2dcTvHKp6aQeKouRRVhMPAspzTJEYmUx9rwjVdNdTa9eEabvsyL1Uq13Wz7WjmbbHa9PBHkHGTNkR2";
-    const decoded = bs58.decode(privKey);
-
+    const myPrivkey = "Qs36P6uJr2dcTvHKp6aQeKouRRVhMPAspzTJEYmUx9rwjVdNdTa9eEabvsyL1Uq13Wz7WjmbbHa9PBHkHGTNkR2";
+    const decoded = bs58.decode(myPrivkey);
     const myKeypair = Keypair.fromSecretKey(decoded);
-    console.log(myKeypair.publicKey);
-    console.log(myKeypair.secretKey);
 
-    const tx = new Transaction();
+    const sendKeypair = Keypair.fromSecretKey(decoded);
+    const receivePubkey = "ES6Pt68agYcG5wVaVPvQh8i3Jd3u6UifRXGtpRdgpptH";
+
+    const tx = new Transaction()
     tx.instructions = [
         SystemProgram.transfer({
-            fromPubkey: myKeypair.publicKey,
-            toPubkey: new PublicKey("ES6Pt68agYcG5wVaVPvQh8i3Jd3u6UifRXGtpRdgpptH"),
+            fromPubkey: sendKeypair.publicKey,
+            toPubkey: new PublicKey(receivePubkey) ,
             lamports: 0.05 * LAMPORTS_PER_SOL,
-            programId: SystemProgram.programId,
-        })];
-    
-    const txHash = await CONNECTION.sendTransaction(tx, [myKeypair]);
+        })
+    ]
+
+    const txHash = await CONNECTION.sendTransaction(tx,[myKeypair]);
     console.log(txHash);
+
+
 })();
